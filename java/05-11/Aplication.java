fab41a7b344cbf8a59514eabea3a91c12c822b3d
Valeria Romero <valellope@gmail.com>
2025-11-06 19:48:23 -0600

    Sobrecarga ademas de this

--- java/05-11/Aplication.java
+++ java/05-11/Aplication.java
@@ -1,13 +1,37 @@
+import java.util.Scanner;
 public class Aplication {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         
         // Crear un objeto de Pokemon (Pikachu)
         Pokemon pikachu = new Pokemon("Pikachu", "Electrico", 120, 50, 90);
+        Pokemon charmander = new Pokemon("Charmander", "Fuego");
+        Pokemon togepi = new Pokemon("Togepi");
 
-        // Mostrar info de los getters
-        System.out.println("Nombre de mi pokemon: " + pikachu.getNombre() + " ademas es tipo: " + pikachu.getTipo());
-        System.out.println("Puntos de salud: (actualmente) " + pikachu.getPuntosSalud());
-        pikachu.setPuntosSalud(140);
-        System.out.println("Nuevos puntos de salud, por usar la capa: " + pikachu.getPuntosSalud());
+        // Mostrar info de los getters 05-11
+        // System.out.println("Nombre de mi pokemon: " + pikachu.getNombre() + " ademas es tipo: " + pikachu.getTipo());
+        // System.out.println("Puntos de salud: (actualmente) " + pikachu.getPuntosSalud());
+        // pikachu.setPuntosSalud(140);
+        // System.out.println("Nuevos puntos de salud, por usar la capa: " + pikachu.getPuntosSalud());
+
+
+        // Mostrar info  06-11
+        pikachu.mostrarInfoPokemon();
+        charmander.mostrarInfoPokemon();
+        togepi.mostrarInfoPokemon();
+
+        // Eclosionar huevo
+        System.out.println("Oh! Este huevo esta a punto de eclosionar " + togepi.getNombre() + " Quieres que eclosione? s/n?");
+        String respuesta = scan.nextLine();
+
+        if(respuesta.equalsIgnoreCase("s")){
+            togepi.eclosionar();
+            togepi.mostrarInfoPokemon();
+        } else{
+            System.out.println("Nuestro seguira incubandose");
+        }
+
+        scan.close();
+
     }
 }
--- java/05-11/Pokemon.java
+++ java/05-11/Pokemon.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Pokemon {
     // Atributos de la clase
     private String nombre;
@@ -5,6 +7,8 @@ public class Pokemon {
     private int puntosSalud;
     private int puntosAtaque;
     private int ataqueEspecial;
+    // Atributo nuevo
+    private boolean enHuevo;
 
     // Constructor
     public Pokemon(String nombre, String tipo, int puntosSalud, int puntosAtaque, int ataqueEspecial) {
@@ -13,6 +17,27 @@ public class Pokemon {
         this.puntosSalud = puntosSalud;
         this.puntosAtaque = puntosAtaque;
         this.ataqueEspecial = ataqueEspecial;
+        this.enHuevo = false;
+    }
+
+    // Constructor sobrecargado 1
+    public Pokemon(String nombre, String tipo) {
+        this(nombre, tipo, 100, 50, 60);
+    }
+
+    // Constructor sin parametros
+    public Pokemon() {
+        this("Desconocido", "Normal");
+    }
+
+    // Constructor para pokemon en huevo
+    public Pokemon(String nombre) {
+        this.nombre = nombre;
+        this.tipo = "Desconocido";
+        this.puntosSalud = 0;
+        this.puntosAtaque = 0;
+        this.ataqueEspecial = 0;
+        this.enHuevo = true;
     }
 
     // Getters y Setters
@@ -49,11 +74,54 @@ public class Pokemon {
         this.puntosAtaque = puntosAtaque;
     }
 
-    public int getAtaqueEspecial(){
+    public int getAtaqueEspecial() {
         return ataqueEspecial;
     }
 
-    public void setAtaqueEspecial(int ataqueEspecial){
+    public void setAtaqueEspecial(int ataqueEspecial) {
         this.ataqueEspecial = ataqueEspecial;
+    }
+
+    // Getters y setters nuevos del atributo de huevo
+    public boolean isEnHuevo() {
+        return enHuevo;
+    }
+
+    public void setEnHuevo(boolean enHuevo) {
+        this.enHuevo = enHuevo;
+    }
+
+    // Metodo de mostrar informacion
+    public void mostrarInfoPokemon() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Tipo: " + tipo);
+        if (enHuevo) {
+            System.out.println("Este huevo aun no ha eclosionado");
+        } else {
+            System.out.println("Salud: " + puntosSalud);
+            System.out.println("Ataque: " + puntosAtaque);
+            System.out.println("Ataque especial: " + ataqueEspecial);
+        }
+        System.out.println("---------------------------------------------------");
+    }
+
+    public void eclosionar() {
+        if (enHuevo) {
+            Random rand = new Random();
+            String[] tipos = {
+                    "Agua", "Fuego", "Psiquico", "Electrico", "Tierra", "Fantasma", "Volador"
+            };
+
+            this.tipo = tipos[rand.nextInt(tipos.length)];
+            this.puntosSalud = rand.nextInt(81) + 20;
+            this.puntosAtaque = rand.nextInt(81) + 20;
+            this.ataqueEspecial = rand.nextInt(81) + 20;
+            this.enHuevo = false;
+
+            System.out.println(nombre + "ha eclosionado del huevo! Woohooo");
+            System.out.println("Tipo asignado es: " + tipo + " Uy esto tal vez no tenga sentido");
+        } else{
+            System.out.println("Oye chico chico " + nombre + " ya habia eclosionado");
+        }
     }
 }
