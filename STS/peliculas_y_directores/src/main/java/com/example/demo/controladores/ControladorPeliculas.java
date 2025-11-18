e686f3fec6f7f0325fe2bc5da1838852ee9756d6
Valeria Romero <valellope@gmail.com>
2025-11-17 20:14:00 -0600

    Peliculas y directores P1

---
+++ STS/peliculas_y_directores/src/main/java/com/example/demo/controladores/ControladorPeliculas.java
@@ -0,0 +1,47 @@
+package com.example.demo.controladores;
+
+import java.util.HashMap;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControladorPeliculas {
+
+	private static HashMap<String, String> listaPeliculas = new HashMap<String, String>();
+	
+	public ControladorPeliculas() {
+		listaPeliculas.put("Winnie the Pooh", "Don Hall");	
+		listaPeliculas.put("El zorro y el sabueso", "Ted Berman");
+		listaPeliculas.put("Tarzán", "Kevin Lima");		
+		listaPeliculas.put("Mulán", "Barry Cook");
+		listaPeliculas.put("Oliver", "Kevin Lima");	
+		listaPeliculas.put("Big Hero 6", "Don Hall");	
+	}
+	
+	@GetMapping("/peliculas")
+	public String obtenerTodasLasPeliculas() {
+		String peliculas = "";
+		
+		for(String pelicula : listaPeliculas.keySet()) {
+			String director = listaPeliculas.get(pelicula);
+			peliculas += pelicula + " - " + director + "<br>"; 
+		}
+		
+		return peliculas;
+	}
+	
+	@GetMapping("/peliculas/{nombre}")
+	public String obtenerPeliculas(@PathVariable String nombre) {
+		for(String pelicula : listaPeliculas.keySet()) {
+			if(pelicula.equalsIgnoreCase(nombre)) {
+				String director = listaPeliculas.get(pelicula);
+				return "Pelicula: " + pelicula + ", Director: " + director;
+			}
+		}
+		
+		return "La película no se encuentra en nuestra lista.";
+	}
+	
+}
