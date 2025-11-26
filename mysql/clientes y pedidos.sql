b2b961e38e2b1b4e6b872d0748b37288c06ef5f6
Valeria Romero <valellope@gmail.com>
2025-11-25 20:09:37 -0600

    reservas de hotel, clientes y pedidos

---
+++ mysql/clientes y pedidos.sql
@@ -0,0 +1,18 @@
+INSERT INTO clientes (nombre, direccion, telefono) VALUES
+('Valeria Romero', 'San Jose', 88880000),
+('Sakura Card Captors', 'Heredia', 88901010),
+('El Mandaloriano', 'Cartago', 70156060),
+('Juan Lopez', 'Puntarenas', 88881010),
+('Miguel Rodriguez', 'San Jose', 85707070);
+
+INSERT INTO pedidos (clientes_id, fecha, total) VALUES 
+(8, '2025-01-01', 15000),
+(1, '2025-01-03', 20000),
+(8, '2025-01-04', 18000),
+(8, '2025-01-10', 22000),
+(3, '2025-01-02', 12000),
+(3, '2025-01-08', 30000),
+(4, '2025-01-05', 25000),
+(5, '2025-01-06', 27000),
+(5, '2025-01-09', 16000),
+(5, '2025-01-11', 21000);
