210091fa538d0d29f44e02b56d46e4a3b42b559a
Valeria Romero <valellope@gmail.com>
2025-11-27 06:59:04 -0600

    script clientes y pedidos

--- mysql/clientes y pedidos.sql
+++ mysql/clientes y pedidos.sql
@@ -6,13 +6,49 @@ INSERT INTO clientes (nombre, direccion, telefono) VALUES
 ('Miguel Rodriguez', 'San Jose', 85707070);
 
 INSERT INTO pedidos (clientes_id, fecha, total) VALUES 
-(8, '2025-01-01', 15000),
-(1, '2025-01-03', 20000),
 (8, '2025-01-04', 18000),
 (8, '2025-01-10', 22000),
-(3, '2025-01-02', 12000),
-(3, '2025-01-08', 30000),
+(2, '2025-01-02', 12000),
+(2, '2025-01-08', 30000),
 (4, '2025-01-05', 25000),
 (5, '2025-01-06', 27000),
 (5, '2025-01-09', 16000),
 (5, '2025-01-11', 21000);
+
+SELECT * FROM clientes JOIN pedidos ON clientes.id = pedidos.clientes_id;
+
+SELECT pedidos.id AS id_pedido, pedidos.fecha, pedidos.total FROM pedidos WHERE pedidos.clientes_id = 8;
+
+SELECT clientes.id, clientes.nombre, SUM(pedidos.total) AS total_pedidos
+FROM clientes
+JOIN pedidos
+ON clientes.id = pedidos.clientes_id
+GROUP BY clientes.id, clientes.nombre;
+
+-- Tuve que borrar la que ya habia añadido --
+ALTER TABLE pedidos
+DROP FOREIGN KEY fk_pedidos_clientes;
+
+-- Para poder agregar el cascade y que el delete pueda borrar incluso cuando hay fk en otras tablas -- 
+ALTER TABLE pedidos
+ADD CONSTRAINT fk_pedidos_clientes
+    FOREIGN KEY (clientes_id)
+    REFERENCES clientes(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE;
+
+DELETE FROM clientes WHERE clientes.id = 2;
+
+SELECT clientes.id, clientes.nombre, COUNT(pedidos.id) AS numero_pedidos 
+FROM clientes
+JOIN pedidos ON clientes.id = pedidos.clientes_id
+GROUP BY clientes.id, clientes.nombre
+ORDER BY numero_pedidos DESC
+LIMIT 3;
+
+
+
+
+
+
+
