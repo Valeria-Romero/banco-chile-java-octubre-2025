e1742aa9db273ac4ec4d7774707f99ebf3193d07
Valeria Romero <valellope@gmail.com>
2025-11-27 20:06:48 -0600

    Canciones parte 1

---
+++ STS/canciones_demo/src/test/java/com/valeriaromero/CancionesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valeriaromero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CancionesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
